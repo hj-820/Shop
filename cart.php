adae5d24ae7b904b243694ac5b5c09b21c6f3ab2
HongJie <145840418+hj-820@users.noreply.github.com>
2025-04-17 01:06:20 +0800

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include 'db.php';
+
+if (!isset($_SESSION['cart'])) $_SESSION['cart'] = [];
+
+if ($_GET['action'] == 'add') {
+    $id = $_GET['id'];
+    $_SESSION['cart'][$id] = ($_SESSION['cart'][$id] ?? 0) + 1;
+}
+
+if ($_GET['action'] == 'remove') {
+    $id = $_GET['id'];
+    unset($_SESSION['cart'][$id]);
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Cart</title>
+</head>
+<body>
+<h2>Your Shopping Cart</h2>
+<?php
+$total = 0;
+foreach ($_SESSION['cart'] as $id => $qty) {
+    $stmt = $conn->prepare("SELECT * FROM products WHERE id=?");
+    $stmt->execute([$id]);
+    $item = $stmt->fetch(PDO::FETCH_ASSOC);
+    $sub = $item['price'] * $qty;
+    $total += $sub;
+    echo "<p>{$item['name']} x $qty = RM $sub 
+          <a href='cart.php?action=remove&id=$id'>Remove</a></p>";
+}
+echo "<h3>Total: RM $total</h3>";
+?>
+<a href="checkout.php">Checkout</a> | <a href="index.php">Continue Shopping</a>
+</body>
+</html>
