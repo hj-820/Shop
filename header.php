78613562fadc0566681657b8241f2c6c644f4791
HongJie <145840418+hj-820@users.noreply.github.com>
2025-04-20 00:10:42 +0800

    Update header.php

--- header.php
+++ header.php
@@ -5,13 +5,22 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header>
-        <h1>🎓 GradElite Graduation Shop</h1>
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="cart.php">View Cart</a>
-        </nav>
+    <header class="header">
+        <div class="logo">
+            <h1>🎓 GradElite Graduation Shop</h1>
+        </div>
+        <div class="navbar">
+            <nav class="nav-left">
+                <a href="index.php">Home</a>
+                <a href="products.php">Products</a>
+                <a href="about.php">About Us</a>
+            </nav>
+            <nav class="nav-right">
+                <a href="cart.php">View Cart</a>
+            </nav>
+        </div>
     </header>
+
     <main>
     </main>
 </body>
