6747b129e17ec0b620f6d41e510a2bc9ed8485b6
HongJie <145840418+hj-820@users.noreply.github.com>
2025-04-20 00:31:14 +0800

    Update products.php

--- products.php
+++ products.php
@@ -1,1 +1,68 @@
+<?php include 'db.php'; ?>
+<?php include 'header.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>GradElite Graduation Shop</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
+<h1 style="margin-left: 20px;">Graduation Products</h1>
+
+<h3 style="margin-left: 40px;">Graduation Flower Bouquet</h3>
+<div class="product-container">
+<?php
+$stmt = $conn->query("SELECT * FROM flowers");
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo "<div class='product-box'>
+            <img src='{$row['img_url']}' alt='{$row['name']}' class='product-img'>
+            <h3>{$row['name']}</h3>
+            <p class='price'>RM {$row['price']}</p>
+            <p class='desc'>{$row['description']}</p>
+            <a class='add-to-cart' href='cart.php?action=add&id={$row['id']}'>Add to Cart</a>
+          </div>";
+}
+?>
+</div>
+
+<h3 style="margin-left: 40px;">Graduation Photo Package</h3>
+<div class="product-container">
+<?php
+$stmt = $conn->query("SELECT * FROM photos");
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo "<div class='product-box'>
+            <img src='{$row['img_url']}' alt='{$row['name']}' class='product-img'>
+            <h3>{$row['name']}</h3>
+            <p class='price'>RM {$row['price']}</p>
+            <ul class='desc'>";
+            
+            foreach (explode("\n", $row['description']) as $line) {
+                echo "<li>" . htmlspecialchars(trim($line)) . "</li>";
+            }
+
+    echo "</ul>
+            <a class='add-to-cart' href='cart.php?action=add&id={$row['id']}'>Add to Cart</a>
+          </div>";
+}
+?>
+</div>
+
+    <h3 style="margin-left: 40px;">Graduation Bears</h3>
+<div class="product-container">
+<?php
+$stmt = $conn->query("SELECT * FROM bears");
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo "<div class='product-box'>
+            <img src='{$row['img_url']}' alt='{$row['name']}' class='product-img'>
+            <h3>{$row['name']}</h3>
+            <p class='price'>RM {$row['price']}</p>
+            <p class='desc'>{$row['description']}</p>
+            <a class='add-to-cart' href='cart.php?action=add&id={$row['id']}'>Add to Cart</a>
+          </div>";
+}
+?>
+</div>
+<?php include 'footer.php'; ?>
+</body>
+</html>
