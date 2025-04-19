59668051d47fa8209b6315beffd3119d53d2b4d0
HongJie <145840418+hj-820@users.noreply.github.com>
2025-04-19 10:19:00 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+    </main>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Graduation Shop. All rights reserved.</p>
+    </footer>
+</body>
+</html>
