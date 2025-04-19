90ee9d3c237f2b179e3bbc2dbf9ea0b3735886db
HongJie <145840418+hj-820@users.noreply.github.com>
2025-04-19 23:19:10 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
     <main>
     </main>
     <footer>
-        <p>&copy; <?php echo date("Y"); ?> Graduation Shop. All rights reserved.</p>
+        <p>&copy; <?php echo date("Y"); ?>GradElite Graduation Shop. All rights reserved.</p>
     </footer>
 </body>
 </html>
