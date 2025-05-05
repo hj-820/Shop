3a7d7dd8d764104ecf9d98091655e09524cd494b
HongJie <145840418+hj-820@users.noreply.github.com>
2025-05-05 13:10:18 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
- $host = var_dump(getenv('DB_HOST'));
- $dbname = getenv('DB_NAME');
- $username = getenv('DB_USER');
- $password = getenv('DB_PASS');
-
+$host = getenv('DB_HOST');
+var_dump($host);  // For debugging, prints the host to confirm it's correct
+$dbname = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
 // Force TCP/IP connection by adding host and port explicitly
 $dsn = "mysql:host=$host;port=3306;dbname=$dbname";
 
