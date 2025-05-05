aa92d551484b3921acf67c9b34d97065766a4725
HongJie <145840418+hj-820@users.noreply.github.com>
2025-05-05 12:41:35 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,15 @@
 <?php
-$host = var_dump(getenv('DB_HOST'));
- $dbname = var_dump(getenv('DB_NAME'));
- $username = var_dump(getenv('DB_USER'));
- $password = var_dump(getenv('DB_PASS'));
+$host = getenv('DB_HOST');
+var_dump($host);  // This should print the new RDS endpoint, not the old one
+ $dbname = getenv('DB_NAME');
+ $username = getenv('DB_USER');
+ $password = getenv('DB_PASS');
 
 // Force TCP/IP connection by adding host and port explicitly
 $dsn = "mysql:host=$host;port=3306;dbname=$dbname";
 
 try {
-echo $host;
+echo ('DB_HOST');
     $conn = new PDO($dsn, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
