7eea31697a13c52922541fbb6ce65b94044a89bb
HongJie <145840418+hj-820@users.noreply.github.com>
2025-05-05 12:33:57 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'shopdb.csbwsu7vetpu.us-east-1.rds.amazonaws.com';
-$dbname = 'shop';
-$username = 'admin';
-$password = 'password';
+$host = getenv('DB_HOST');
+ $dbname = getenv('DB_NAME');
+ $username = getenv('DB_USER');
+ $password = getenv('DB_PASS');
 
 // Force TCP/IP connection by adding host and port explicitly
 $dsn = "mysql:host=$host;port=3306;dbname=$dbname";
