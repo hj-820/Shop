a01b0782c05790ec38aac8c00bd8a49a8dab62c6
HongJie <145840418+hj-820@users.noreply.github.com>
2025-05-05 12:37:26 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,14 @@
 <?php
-$host = getenv('DB_HOST');
- $dbname = getenv('DB_NAME');
- $username = getenv('DB_USER');
- $password = getenv('DB_PASS');
+$host = var_dump(getenv('DB_HOST'));
+ $dbname = var_dump(getenv('DB_NAME'));
+ $username = var_dump(getenv('DB_USER'));
+ $password = var_dump(getenv('DB_PASS'));
 
 // Force TCP/IP connection by adding host and port explicitly
 $dsn = "mysql:host=$host;port=3306;dbname=$dbname";
 
 try {
+echo $host;
     $conn = new PDO($dsn, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
