b34e6b1335756876821f087133dee9d349e5bccf
HongJie <145840418+hj-820@users.noreply.github.com>
2025-05-05 13:40:28 +0800

    Update db.php

--- db.php
+++ db.php
@@ -8,7 +8,6 @@ $password = getenv('DB_PASS');
 $dsn = "mysql:host=$host;port=3306;dbname=$dbname";
 
 try {
-echo $host;
     $conn = new PDO($dsn, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
