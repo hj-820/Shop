fa88e4ac2eee607b00b79ba7bfe31caff1f95969
HongJie <145840418+hj-820@users.noreply.github.com>
2025-05-05 13:34:52 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$host = getenv('DB_HOST');
+$host = 'shopdb.csbwsu7vetpu.us-east-1.rds.amazonaws.com';
 $dbname = getenv('DB_NAME');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASS');
