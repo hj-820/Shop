568fea10d3962a33100b6f3489e0895a9469430d
HongJie <145840418+hj-820@users.noreply.github.com>
2025-05-05 13:00:57 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,5 @@
 <?php
 $host = getenv('DB_HOST');
-var_dump($host);  // This should print the new RDS endpoint, not the old one
  $dbname = getenv('DB_NAME');
  $username = getenv('DB_USER');
  $password = getenv('DB_PASS');
@@ -9,7 +8,7 @@ var_dump($host);  // This should print the new RDS endpoint, not the old one
 $dsn = "mysql:host=$host;port=3306;dbname=$dbname";
 
 try {
-echo ('DB_HOST');
+echo $host;
     $conn = new PDO($dsn, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
